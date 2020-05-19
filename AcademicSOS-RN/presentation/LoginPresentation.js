f9a72fb841b73cca70a8dc8b0b1579fb69c86a1f
Marcus Lee Eugene <mctjl_1997@yahoo.com.sg>
2020-05-19 13:49:37 +0800

    Login Presentation added in font for AcademicSOS

--- AcademicSOS-RN/presentation/LoginPresentation.js
+++ AcademicSOS-RN/presentation/LoginPresentation.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
 import { useFonts } from '@use-expo/font';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { AppLoading } from 'expo';
 
 export default function LoginPresentation() {
-  props => {
-    let [fontsLoaded] = useFonts({
-      'SonsieOne-Regular': require('../assets/fonts/SonsieOne-Regular.ttf'),
-    });
-  }
 
-  return (
-    <View style = {styles.container}>
-      <Text style = {styles.font}>AcademicSOS</Text>
-      <Image style = {styles.nusLogo} source={require('../images/NUS_logo_full-horizontal.jpg')} />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    'SonsieOne-Regular': require('../assets/fonts/SonsieOne-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style = {styles.container}>
+        <Text style = {styles.font}>AcademicSOS</Text>
+        <Image style = {styles.nusLogo} source={require('../images/NUS_logo_full-horizontal.jpg')} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -23,9 +26,8 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   font: {
-    fontSize: 50,
+    fontSize: 35,
     textAlign: "center",
-    margin: 10,
     fontFamily: "SonsieOne-Regular"
   }
 
