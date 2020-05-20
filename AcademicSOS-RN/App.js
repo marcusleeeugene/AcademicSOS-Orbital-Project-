3fea17c9c6b1be930407ddd5e320f933e26a21df
Marcus Lee Eugene <mctjl_1997@yahoo.com.sg>
2020-05-20 16:22:40 +0800

    Edited prototype "Book consultation"

--- AcademicSOS-RN/App.js
+++ AcademicSOS-RN/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import LoginPresentation from './presentation/LoginPresentation.js'
-import SelectModulePresentation from './presentation/SelectModulePresentation.js'
+//import SelectModulePresentation from './presentation/SelectModulePresentation.js'
 
 export default function App() {
   return (
