348df6590e1a2f2554817e928589c751f25aa601
Tay Kai Xiang <taykaixiang97@gmail.com>
2020-05-20 21:01:39 +0800

    completed BookConsultScreen and HomeScreen
    added in breadcrumbs to header, scrollView and limit characters, number of lines in remarks textbox

--- AcademicSOS-RN/App.js
+++ AcademicSOS-RN/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import LoginPresentation from './presentation/LoginPresentation.js'
-import SelectModulePresentation from './presentation/SelectModulePresentation.js'
-import BreadCrumb from './presentation/BreadCrumb.js'
+import React from "react";
+import LoginPresentation from "./presentation/LoginPresentation.js";
+import SelectModulePresentation from "./presentation/SelectModulePresentation.js";
+import BreadCrumb from "./presentation/BreadCrumb.js";
+import BookConsultScreen from "./presentation/BookConsultScreen.js";
+import HomeScreen from "./presentation/HomeScreen.js";
 
 export default function App() {
   return (
-    //<LoginPresentation></LoginPresentation>
-    <SelectModulePresentation></SelectModulePresentation>
-    //<BreadCrumb></BreadCrumb>
+    // <LoginPresentation></LoginPresentation>
+    // <SelectModulePresentation></SelectModulePresentation>
+    // <BreadCrumb></BreadCrumb>
+    <BookConsultScreen></BookConsultScreen>
+    // <HomeScreen></HomeScreen>
   );
 }
