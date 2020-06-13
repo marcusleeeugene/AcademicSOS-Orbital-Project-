95a0309c53d94bd1dc9ec4099740d2ff575da93a
Marcus Lee Eugene <mctjl_1997@yahoo.com.sg>
2020-06-13 19:58:18 +0800

    latest of latest

--- AcademicSOS-RN/firebase/ManageBookingFireBase.js
+++ AcademicSOS-RN/firebase/ManageBookingFireBase.js
@@ -25,7 +25,6 @@ function compareTime(a, b) {
   } else if (firstTime < secondTime) {
     comparison = -1;
   }
-  console.log(comparison);
   return comparison;
 }
 
@@ -108,7 +107,7 @@ const ManageBookingFB = {
             return selectedWeek == selectedDate.diff(startingWeek, "weeks") || selectedWeek === "Weeks";
           })
           .filter((rsl) => { //Filter by day
-            var bookDay = moment(rsl.date).format("dddd");
+            var bookDay = moment(rsl.consultDate).format("dddd");
             return bookDay === day || day === "All Days";
           });
       });
