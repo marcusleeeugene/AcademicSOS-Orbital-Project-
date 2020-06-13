6ebce6ca720dfd1d0efc85f34cd11118983bd732
Marcus Lee Eugene <mctjl_1997@yahoo.com.sg>
2020-06-13 15:09:23 +0800

    Update BookConsultFireBase.js

--- AcademicSOS-RN/firebase/BookConsultFireBase.js
+++ AcademicSOS-RN/firebase/BookConsultFireBase.js
@@ -1,22 +1,6 @@
 import * as firebase from "firebase";
 import { database, role } from "./FireBaseConfig.js";
 
-function WeekRange() { //get a promise for week range within acad year based on real time later on.
-  return fetch('https://api.nusmods.com/v2/2019-2020/modules/CS2040.json') //This part, to be made dynamic in future
-    .then((result) => result.json())
-    .then((data) => {
-      var semData = data['semesterData'];
-      var weekRange;
-      for (var sem in semData) {
-        if ("start" in semData[sem]['timetable'][0]['weeks']) {
-          weekRange = semData[sem]['timetable'][0]['weeks']
-          break;
-        }
-      }
-      return weekRange;
-    });
-}
-
 const BookConsultFB = {
   getWeekRange: function() {
     return fetch('https://api.nusmods.com/v2/2019-2020/modules/CS2040.json') //This part, to be made dynamic in future
